01f6fe856bde55da9dad0bb3bbee1a45f84369f0
manor hagage <manorhagage@gmail.com>
2021-02-22 15:16:03 +0200

    Add findFolder function

--- public/index.js
+++ public/index.js
@@ -7,3 +7,18 @@ function __( selector )
 {
     return document.querySelectorAll( selector );
 }
+
+function findFolder( currentFolder, folderName )
+{
+	let folder = false
+	let counter = 0;
+	currentFolder.forEach( e => 
+	{
+		if( e.name === folderName )
+		{
+			folder = currentFolder[counter]
+		}
+		counter++;
+	});
+	return folder;
+}
