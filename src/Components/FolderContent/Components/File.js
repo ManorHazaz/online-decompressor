3b1db4b5b8dc9d4f89ca09012f3fe0e5efd9a175
manor hagage <manorhagage@gmail.com>
2021-02-23 14:22:02 +0200

    Change files logos

--- src/Components/FolderContent/Components/File.js
+++ src/Components/FolderContent/Components/File.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-import fileText from '../../../Static/text.svg';
-import fileImage from '../../../Static/picture.svg';
-import fileDll from '../../../Static/dll.svg';
+import fileText from '../../../Static/txt.svg';
+import fileJpg from '../../../Static/jpg.svg';
+import filePng from '../../../Static/png.svg';
 import fileCss from '../../../Static/css.svg';
-import fileJs from '../../../Static/js.svg';
+import fileJs from '../../../Static/javascript.svg';
 import fileHtml from '../../../Static/html.svg';
-import fileDefault from '../../../Static/default.svg'
+import fileDefault from '../../../Static/file.svg';
+import fileJson from '../../../Static/json-file.svg';
+import fileMp3 from '../../../Static/mp3.svg';
+import fileDoc from '../../../Static/doc.svg';
+import filePdf from '../../../Static/pdf.svg';
+import fileZip from '../../../Static/zip.svg';
 
 function File({ file }) 
 {
@@ -16,20 +21,14 @@ function File({ file })
             case 'txt':
                 return fileText;
             
-            case 'gif':
-                return fileImage;
-            
             case 'png':
-                return fileImage;
+                return filePng;
             
             case 'jpg':
-                return fileImage;
+                return fileJpg;
             
-            case 'peng':
-                return fileImage;
-            
-            case 'dll':
-                return fileDll;
+            case 'json':
+                return fileJson;
             
             case 'html':
                 return fileHtml;
@@ -37,13 +36,27 @@ function File({ file })
             case 'css':
                 return fileCss;
             
+            case 'scss':
+                return fileCss;
+            
             case 'js':
                 return fileJs;
+            
+            case 'mp3':
+                return fileMp3;
+            
+            case 'doc':
+                return fileDoc;
+            
+            case 'pdf':
+                return filePdf;
+            
+            case 'zip':
+                return fileZip;
             
             default:
                 return fileDefault;
         }
-
     }
 
     return (
